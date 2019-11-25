8ac0a3e4a70e9600c88d7a9146ee3a4c5a71e6f7
alanwake47 <aman.krishna4@gmail.com>
2019-11-25 16:28:47 +0530

    lpthw2

---
+++ LP3THW/ex07.py
@@ -0,0 +1,14 @@
+print('mary had a little lanb.')
+print('its fleece was white as {}.'.format('sno'))
+print('and everywhere that mary went.')
+print('.'*10)
+
+end1='C'
+end2='h'
+end3='e'
+end4='ese'
+end5="bur"
+end6="ger"
+
+print(end1+end2+end3+end4, end=' ')
+print(end5+end6)
